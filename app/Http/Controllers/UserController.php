28dd294164f9b28b45317cbbf3196fe91afe4be9
Irum Zaidi <irum@artynode.com>
2025-08-15 21:36:38 +0500

    Form Request Validation (28/64 lecture)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,36 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
 
-    public function addUser(Request $req)
+    public function addUser(UserRequest $req)
     {
 
-        $req->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|alpha_num|min:6',
-            // 'age' => 'required|numeric|min:18|max:100',
-            'age' => 'required|numeric|between:28,100',
-            'city' => 'required',
+        // $req->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => 'required|alpha_num|min:6',
+        //     // 'age' => 'required|numeric|min:18|max:100',
+        //     'age' => 'required|numeric|between:28,100',
+        //     'city' => 'required',
 
-            // 'name' => 'required|string|max:255',
-            // 'email' => 'required|email|unique:users,email',
-            // 'password' => 'required|string|min:8|confirmed',
-        ], [
-            'name.required' => 'Please enter your name',
-            'email.required' => 'Please enter your email address',
-            'password.required' => 'Please enter a password',
-            'age.required' => 'Please enter your age. min age 28',
-            'city.required' => 'Please select a city',
-            // You can customize the error messages here            
-        ]);
-        return $req->all();
+        //     // 'name' => 'required|string|max:255',
+        //     // 'email' => 'required|email|unique:users,email',
+        //     // 'password' => 'required|string|min:8|confirmed',
+        // ], [
+        //     'name.required' => 'Please enter your name',
+        //     'email.required' => 'Please enter your email address',
+        //     'password.required' => 'Please enter a password',
+        //     'age.required' => 'Please enter your age. min age 28',
+        //     'city.required' => 'Please select a city',
+        //     // You can customize the error messages here            
+        // ]);
+        // return $req->all();
+        
+        
         // Return the user form view
         // return view('user-form');
+        
+        // return $req->all();
+
+        // return $req->only('name', 'city');
+        return $req->except('name', 'city');
     }
+
+
 }
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // return false;
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|alpha_num|min:6',
+            'age' => 'required|numeric|between:18,100',
+            'city' => 'required',
+
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            // 'name.required' => 'Please enter your name',
+            'name.required' => 'Please enter your :attribute  .... ',
+            'email.required' => 'Please enter your email address',
+            'password.required' => 'Please enter a password',
+            'age.required' => 'Please enter your age. min age 18',
+            'city.required' => 'Please select a city',
+            // You can customize the error messages here            
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'user name',
+            'email' => 'email address',
+            'password' => 'user account password',
+            'age' => 'user age',
+            'city' => 'user city',
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        // You can modify the request data before validation if needed
+        // For example, you can trim whitespace from the name
+        $this->merge([
+            // 'name' => trim($this->name),
+            // 'name' => strtoupper($this->name),
+            'name' => Str::slug($this->name),
+        ]);
+    }
+
+    // protected $stopOnFirstFailure = true;
+}
